7c04f35fe1cdf6d9fadbcee9a24ac536b74af4b1
Aldo510 <aldo.619_@ciencias.unam.mx>
2022-04-01 16:10:49 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Aldo Márquez Aguilar on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
